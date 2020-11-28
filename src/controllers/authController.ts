6a0f84b7dc2e30583372f787e39ce6e21cd1cb2d
Kaden <cartwrightkaden@gmail.com>
2020-11-28 12:05:15 -0600

    refactored router logic partially into controllers

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,26 +1,21 @@
-import { TokenInterface } from '../interfaces/tokenInterface';
-import { LoginInterface } from '../interfaces/loginInterface';
-import { Role } from '../models/Role';
-import { User } from '../models/User';
-import {Router}  from 'express';
-import {check, validationResult} from 'express-validator'
-import {Container} from 'typedi';
+import { User } from './../models/User';
+import { Role } from './../models/Role';
+import { TokenInterface } from './../interfaces/tokenInterface';
+import { LoginInterface } from './../interfaces/loginInterface';
+import {Request,Response} from 'express';
+import { Container } from 'typedi';
+import { validationResult } from 'express-validator';
 import AuthService from '../services/AuthService';
 import UserService from '../services/UserService';
-var router: Router = Router();
 
-
-router.post('/login',[
-    check('email').isEmail(),
-    check('password').isString(),
-], async(req,res)=>{
+let login: (req:Request,res:Response)=>void = async function(req,res){
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
     let authService = Container.get(AuthService)
     let userService = Container.get(UserService)
-    let loginData:LoginInterface = {
+    let loginData:LoginInterface= {
         email: req.body.email,
         password: req.body.password
     }
@@ -38,31 +33,19 @@ router.post('/login',[
             lastName: user.lastName,
             roles: user.roles
         }
-
         res.status(200).json({
             token:token,
             user: userJson
         })
-
     }
-    
-})
+}
 
-
-router.post('/create-user',[
-    check('email').isEmail(),
-    check('password').isString(),
-    check('password').isLength({min:8}),
-    check('phoneNumber').isMobilePhone('any'),
-    check('firstName').isString(),
-    check('lastName').isString()
-],async (req,res)=>{
+let createUser:(req:Request,res:Response)=>void= async (req:Request,res:Response)=>{
     //if not all validations pass, return status code 400 and the output from validation result
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-
     //get an authservice instance  
     let authService = Container.get(AuthService)
     let roles:Role[];
@@ -71,8 +54,7 @@ router.post('/create-user',[
     authService.createUser(user);
     res.send('done')
     
-})
+}
 
 
-
-export = router;
+export {login, createUser}
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,14 +1,57 @@
 import {Router}  from 'express';
-import apiRouter from './api.router'
-
+import { User } from '../models/User';
+import { Role } from '../models/Role';
+import {check, validationResult} from 'express-validator'
+import { TokenInterface } from '../interfaces/tokenInterface';
+import { LoginInterface } from '../interfaces/loginInterface';
+import {Container} from 'typedi';
+import AuthService from '../services/AuthService';
+import UserService from '../services/UserService';
+import * as apiController from '../controllers/apiController'
+import * as authController from'../controllers/authController'
 var router: Router = Router();
+var apiRouter: Router = Router();
 
+
+/**
+ * Root Routes
+ */
 router.get('/', (req,res)=>{
     res.send('running')
 })
 
+    /**
+     * API Routes (/api/v1/*) 
+     */
+    router.use('/api/v1', apiRouter)
+    apiRouter.get('/get-session',
+    [
+        check('id').isString(),
+        check('id').isLength({min:36,max:36})
+    ],apiController.getSession)
 
-router.use('/api/v1', apiRouter)
+        /**
+         * Auth Routes
+         * (/api/v1/auth/*) 
+         */
+        apiRouter.use('/auth', authRouter);
+        var authRouter: Router = Router();
+
+
+        authRouter.post('/login',[
+            check('email').isEmail(),
+            check('password').isString(),
+        ], authController.login)
+
+
+        authRouter.post('/create-user',[
+            check('email').isEmail(),
+            check('password').isString(),
+            check('password').isLength({min:8}),
+            check('phoneNumber').isMobilePhone('any'),
+            check('firstName').isString(),
+            check('lastName').isString()
+        ],authController.createUser)
 
 
 export = router;
